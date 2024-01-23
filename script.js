363fa0b96f0431a9fae26087d839f886db7f81ef
Eugenio <eugenio.spera@gmail.com>
2024-01-23 14:16:42 -0300

    exeercicio time de futebol

---
+++ script.js
@@ -0,0 +1,16 @@
+let num = parseInt(prompt("Escolha um time: " + "\n" + "1- Real Madrid" + "\n" + "2- Barcelona" + "\n" + "3- PSG"))
+
+switch(num){
+    case 1:
+        alert("Real madrid, boa escolha!")
+        btreak;
+    case 2:
+        alert("O lendário Barcelona")
+        break;
+    case 3:
+        alert("PSG") 
+        break;   
+
+    default:
+        alert("Volte e escolha um número de 1 a 3")
+} 
